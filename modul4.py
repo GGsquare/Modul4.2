1c49e532f5d7c6bb1743a66b86d11a4bb367ef49
Grzegorz Gojke <ggojke@gmail.com>
2022-02-26 19:25:52 +0100

    rozszerzenie definicji

---
+++ modul4.py
@@ -0,0 +1,19 @@
+shopping_items = [
+    "jajka",
+    "bułka",
+    "ser feta",
+    "masło",
+    "pomidor",
+    "chusteczki",
+    "papier toaletowy"
+]
+
+def shopping(items, payment='card', shop='local'):
+    pass
+    shopping_cart = "Koszyk zawiera: "
+    for item in items:
+        shopping_cart += item + '\n'
+    return shopping_cart
+
+basket = shopping(shopping_items)
+print(basket)
