d7f3720e804218173de12190add9b3eeb833af5f
Grzegorz Gojke <ggojke@gmail.com>
2022-02-28 09:14:59 +0100

    Add split function

--- modul4.py
+++ modul4.py
@@ -12,7 +12,7 @@ def shopping(items, payment='card', shop='local'):
     shopping_cart = "Koszyk zawiera: "
     for item in items:
         shopping_cart += item + ", "
-    return shopping_cart, payment, shop
+    return shopping_cart.split('\n'), payment, shop
 
 return_data = shopping(shopping_items)
 shoping_cart, payment, shop = return_data
